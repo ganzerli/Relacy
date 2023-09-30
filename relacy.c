2a30b77eb40f32c672fcf6f7fcde7610fba5ccc1
name <massimoganzerli@yahoo.com>
2023-09-30 22:05:40 +0200

    first commit

---
+++ relacy.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void main(){
+    printf("#################################################################\n");
+    printf("#                                                               #\n");
+    printf("#                       WELCOME TO RELACY                       #\n");
+    printf("#   Relacy is an interface connecting to e-paty.                #\n");
+    printf("#   With relacy you can navigate in e-pathy througt paths.      #\n");
+    printf("#   Create a database with e-paty.                              #\n");
+    printf("#   With relacy the research for words is extremely fast,       #\n");
+    printf("#   the shape of the databse is not even a question,            #\n");
+    printf("#   humans does not apply to relacy, relacy applies to humnas   #\n");
+    printf("#   Tecknology should help, relacy is one of the answers.       #\n");
+    printf("#                                                               #\n");
+    printf("#################################################################\n");
+
+
+
+}
