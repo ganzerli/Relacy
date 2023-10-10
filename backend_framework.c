4b7bd5e8cbd59eed387edb92fb1372443731bede
name <massimoganzerli@yahoo.com>
2023-10-11 00:32:31 +0200

    router

--- backend_framework.c
+++ backend_framework.c
@@ -42,15 +42,7 @@ unsigned int backend_framework(char* buffer , unsigned int size){
         unsigned int request_size = size;
         unsigned int response_size = size;
 
-        struct HTTP_request{
-                char* method;
-                char* URI;
-                char* HTTP_version;
-                unsigned int header_count;
-                char** headers;
-                unsigned int attributes_count;
-                char* body;
-        }http_request;
+        struct HTTP_request http_request;
 
         // manage request
         manage_request(buffer , size , &http_request);
--- request.c
+++ request.c
@@ -1,36 +1,30 @@
 
+struct HTTP_request{
+    char* method;
+    char* URI;
+    char* HTTP_version;
+    unsigned int headers_count;
+    char** headers;
+    unsigned int attributes_count;
+    char* body;
+};
 
-void manage_request(char *buffer , unsigned int size , struct HTTP_request* http_request){
+void manage_request(char *buffer , unsigned int size , struct HTTP_request * req){
     buffer[size] = '\0';
 
     char** request = str_split( "\n" , buffer );                                // str_split allocates the memory!!
     
-    char* first_line = request[0];                                            // separate first line of request from rest
-    char** headers = &request[1];                                              // keep only headers
-    unsigned int headers_count = get_strgs_counter()-1;                        // remember count of the headers -> str_split sets counter
+    char* first_line = request[0];                                              // separate first line of request from rest
+    char** headers = &request[1];                                               // keep only headers
+    unsigned int headers_count = get_strgs_counter()-1;                         // remember count of the headers -> str_split sets counter
                                                                                     
-    char** first_line_ = str_split(" " , first_line);                          // split first line
-    //set_strgs_counter(3);
-    //print_strings(first_line_);
+    char** first_line_ = str_split(" " , first_line);                           // split first line
 
-    const char* METHOD = first_line_[0];
-    const char* URI = first_line_[1];
-    const char* HTTP_VERSION = first_line_[2];
-
-
-    //printf("last header: %s \n" , headers[headers_count-1]);
-
-    http_request->method  = METHOD;
-    http_request->URI  = URI;
-    http_request->HTTP_version  =  HTTP_VERSION ;
-    http_request->headers_count  = headers_count ;
-    http_request->headers  = headers ;
-    http_request->body  =  headers[headers_count-1];
-
-    // str_split & print_strings are not good nested
-    //set_strgs_counter(headers_count);
-    //print_strings(headers);
+    req->method  = first_line_[0];
+    req->URI  = first_line_[1];
+    req->HTTP_version  =  first_line_[2] ;
+    req->headers_count  = headers_count ;
+    req->headers  = headers ;
+    req->body  =  headers[headers_count-1];
 
 }
-
-
