335ff714e7c5862172ef405860ff3daa8c98cbea
name <massimoganzerli@yahoo.com>
2023-10-24 23:46:36 +0200

    free heap for all strings allocated on request

--- backend_framework.c
+++ backend_framework.c
@@ -2,70 +2,54 @@
 #include "request.c"
 #include "response.c"
 
+// CONTROLLERS
+#include "controllers.c"
+
+// ROUTER
 u8 found;
+void route(char* route , void(*controller)() );
+void router();
 
-void ctrl_favicon(){
-        response_send_pic("E-pachy_512x512.png");
 
+
+
+// DEFINE HERE YOUR ROUTES ( URI , CONTROLLER )
+void get(){
+        route( "/" , ctrl_home );
+        route( "/favicon.ico" , ctrl_favicon );
 }
 
-// controllers from other file
-void ctrl_home(){
-
-        add_response_header("Server: E-pache 1.0");
-        response_send_file("views/html.html");
-
-
+void post(){
+        route("/" , ctrl2 );
 }
 
-void ctrl2(){
+// NEEDED TO FREE REQUEST STRINGS
+unsigned int backend_framework(char* buffer , unsigned int size){
+        unsigned int request_size = size;
+        manage_request(buffer , size );
+        // use same buffer also for response
+        response_config(buffer);
+        router();
+        // free all the memory allocated for strings in request
+        str_free_all();
+        return http_response.response_length;;
 }
 
 
+// ROUTER
 void route(char* route , void(*controller)() ){
         if(found) return;
-
-
         int _len = str_len(http_request.URI);
-
         if(str_cmp(_len , http_request.URI, route)){
                 found = 1;
                 controller();
         }
 
 }
-
-///////////// defining
-void get(){
-
-        route( "/" , ctrl_home );
-        route( "/favicon.ico" , ctrl_favicon );
-
-}
-
-void post(){
-
-        route("/" , ctrl2 );
-
-}
-
-
 void router(){
         found = 0;
         char* method = http_request.method;
         if( str_cmp(3 , "GET" ,method) ) get();
         if( str_cmp(4 , "POST" ,method) ) post();
+        // all the others are the same
 };
-
-
-// size unsigned int is good until 4GB
-unsigned int backend_framework(char* buffer , unsigned int size){
-
-        unsigned int request_size = size;
-        manage_request(buffer , size , &http_request);
-        // use same buffer also for response
-        response_config(buffer);
-        router();
-
-        return http_response.response_length;;
-}
